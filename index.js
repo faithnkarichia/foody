2f22cf7632b66bc5080848ee889eb1c1b7c07787
Vincent Muriuki <vincent.muriuki@bskglobaltech.com>
2020-09-15 07:59:00 -0700

    added cities and customer review section

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let liveServer = require("live-server")
 let params = {
     port: 8080,
     host: "127.0.0.1",
-    file: "index.html"
+    file: "home.html"
 }
 liveServer.start(params)
+// nodemon index.js
